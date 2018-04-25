c77ecdfe52dace1abc115ba589e2207c69d5848b
TheCodeWorm <4purposeliving@gmail.com>
2018-04-25 11:21:39 -0700

    u

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 
-var request = require('request');0
-var cheerio = require('cheerio');
+let request = require('request');0
+let cheerio = require('cheerio');
 
 request('https://www.blueletterbible.org/nkjv/rom/14', function(err, resp, html) {
 	if (!err){
-	  var $ = cheerio.load(html);
+	  let $ = cheerio.load(html);
 		$('div.tools').each(function(i, element){
-			var a = $(this);
-			a = a.text().split('\n');
-			a = a[9];
-			a = a.split('\t');
+			let a = $(this).text().split('\n');
+			a = a[9].split('\t');
 			a = a[4];
 			a = a.replace('¶', '');
 			a = a.replace('[fn]', '');
 			a = a.split('-');
-			var verse = a[0];
-			var passage = '';
-			for (var i=1; i<a.length; i++ ) {
+			let verse = a[0];
+			let passage = '';
+			for (let i=1; i<a.length; i++ ) {
 				passage += a[i];
 			}
 			passage = passage.trim();
-			console.log(verse);
-			console.log(passage);
-			console.log('');
+			let arr = passage.split(' ');
+			let tmpStr = '';
+			let strLen = 0;
+			for (let i=0; i<arr.length; i++) {
+				strLen += arr[i].length;
+				if (strLen > 27) {
+					tmpStr += '\n';
+					strLen = arr[i].length;
+				}
+				tmpStr += arr[i] + ' ';
+				strLen += 1;
+	    }
+   		passage = tmpStr;
+			console.log('\n' + verse + '\n' + passage);
     });
   }
 });
-
 
