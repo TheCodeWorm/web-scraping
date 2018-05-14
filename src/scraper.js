3ef0accb1285e68f2c9bb8c7d94a2d2e9fb9e361
TheCodeWorm <4purposeliving@gmail.com>
2018-05-14 16:34:14 -0700

    commit to save

--- src/scraper.js
+++ src/scraper.js
@@ -5,12 +5,57 @@ import React, { Component } from 'react';
 //import './scraper.css';
 //import App from './App';
 
+const request = require('request');
+const cheerio = require('cheerio');
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+const myUrl = 'https://www.blueletterbible.org/nkjv/rom/14';
+
+function scraper() {
+	let versesParagraph = document.createElement("p");
+	request(proxyUrl + myUrl, function(err, resp, html) {
+		let allVerses = ""
+		if (!err){
+		  let $ = cheerio.load(html);
+			$('div.tools').each(function(i, element){
+				let a = $(this).text().split('\n');
+				a = a[9].split('\t');
+				a = a[4];
+				a = a.replace('¶', '');
+				a = a.replace('[fn]', '');
+				a = a.split('-');
+				let verse = a[0];
+				let passage = '';
+				for (let i=1; i<a.length; i++ ) {
+					passage += a[i];
+				}
+				passage = passage.trim();
+				let arr = passage.split(' ');
+				let tmpStr = '';
+				let strLen = 0;
+				for (let i=0; i<arr.length; i++) {
+					strLen += arr[i].length;
+					if (strLen > 27) {
+						tmpStr += '\n';
+						strLen = arr[i].length;
+					}
+					tmpStr += arr[i] + ' ';
+					strLen += 1;
+		    }
+        
+	   		allVerses += '\n' + verse + '\n' + tmpStr + '\n';
+	    });
+	    //console.log(allVerses);
+	    versesParagraph.appendChild(document.createTextNode(allVerses));
+	  }
+	})
+}
+
 class Scraper extends Component {
   render() {
     return (
       <div>
         <p>
-          testing Scraper
+          testing {("results: " + console.log())}
         </p>
       </div>
     );
@@ -18,46 +63,3 @@ class Scraper extends Component {
 }
 
 export default Scraper;
-
-
-let request = require('request');
-let cheerio = require('cheerio');
-
-let myUrl = 'https://www.blueletterbible.org/nkjv/rom/14';
-
-let myreq = request(myUrl, function(err, resp, html) {
-	let arr = [];
-	if (!err){
-	  let $ = cheerio.load(html);
-		$('div.tools').each(function(i, element){
-			let a = $(this).text().split('\n');
-			a = a[9].split('\t');
-			a = a[4];
-			a = a.replace('¶', '');
-			a = a.replace('[fn]', '');
-			a = a.split('-');
-			let verse = a[0];
-			let passage = '';
-			for (let i=1; i<a.length; i++ ) {
-				passage += a[i];
-			}
-			passage = passage.trim();
-			let arr = passage.split(' ');
-			let tmpStr = '';
-			let strLen = 0;
-			for (let i=0; i<arr.length; i++) {
-				strLen += arr[i].length;
-				if (strLen > 27) {
-					tmpStr += '\n';
-					strLen = arr[i].length;
-				}
-				tmpStr += arr[i] + ' ';
-				strLen += 1;
-	    }
-   		passage = '\n' + verse + '\n' + tmpStr;
-			console.log(passage);
-			arr.push(passage);
-    });
-  }
-  return arr;
-});
