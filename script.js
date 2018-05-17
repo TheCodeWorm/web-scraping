ccd63209246c41255a12fb245aced124297c125f
TheCodeWorm <4purposeliving@gmail.com>
2018-05-17 16:16:18 -0700

    commit save

--- script.js
+++ script.js
@@ -3,22 +3,62 @@
 const myProxy = 'https://cors-anywhere.herokuapp.com/';
 const myURL = 'https://cors-anywhere.herokuapp.com/https://www.blueletterbible.org/nkjv/rom/14/1/';
 
+let book = "rom";
+let chapter = "14";
+let toSearchFor = '<a href="/nkjv/' + book + '/' + chapter;
+
 fetch(myProxy + myURL).then(function(res) {
-	return res.text();
+	return res.text();  // convert to text
 }).then(function(html) {
-	const results = html.match(/Rom(.*)div/g);
-	const resLen = results.length;
-	let verses = [resLen];
-	let scriptures = [resLen];
-	for( let i=0; i< resLen; i++) {
-		//console.log(results[i]);
-		verses[i] = results[i].match(/.+?(?=<)/)[0];
-		//scriptures[i] = results
-		console.log(verses[i]);
-		console.log(results[i]);
+
+  ////// Get verses from html:
+	let verses = [];  	// array for final extracted verses
+	// get results from html, to begin extacting verses, Ex: Rom 14:1
+  let resultsForVerses = html.match(/<(.*)>/g);
+
+  // search results for verses
+  for( let i=0; i< resultsForVerses.length; i++) {
+  	if (resultsForVerses[i].includes(toSearchFor)) {
+  		verses.push(resultsForVerses[i].match(/(>).+?(?=<)/)[0].replace('>', ''));
+  	}
 	}
-	//var element = document.getElementById('myImgElement');
+
+	for( let i=0; i< verses.length; i++) {
+  	console.log(verses[i]);
+  }
+
+  ////// Get passages from html:
+  let passages = [];  // array for final extracted passages
+	// get results from html, for the passages of verses, 
+	// Ex: Receive one who is weak in the faith, but not to disputes over doubtful things.
+	let resultsForPassages = html.match(/hide-for-tablet"> - <\/span>(.*)(<\/div><\/div>)/g);
+	
+	for( let i=0; i< resultsForPassages.length; i++) {
+		resultsForPassages[i] = resultsForPassages[i].match(/(?<=\/span>)(.*)(?=<\/div><\/div>)/g)[0];
+		resultsForPassages[i] = removeBrackets(resultsForPassages[i]);
+		resultsForPassages[i] = resultsForPassages[i].replace("&#8220;", '"').replace("&#8221;", '"');
+		passages.push(resultsForPassages[i].replace("&#8217;", "'").replace("[fn]", ""));
+		console.log(passages[i]);
+	}
 });
+
+// remove everything inside brackets, including brackets
+function removeBrackets(str) {
+	let result = "";
+	let isInBracket = false   // if in bracket, including bracket
+  for (let i=0; i < str.length; i++) {
+  	if (str[i] === '<') {
+  		isInBracket = true;
+  	}
+  	else if (str[i] === '>') {
+  		isInBracket = false;
+  	}
+    else if (isInBracket === false) {
+    	result +=  str[i];
+    }
+  }
+  return result;
+}
 
 /*
 let versesParagraph = document.createElement("p");
