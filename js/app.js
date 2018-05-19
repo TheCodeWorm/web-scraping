b9982486091f456663281e4f00535f7e1a11642f
TheCodeWorm <4purposeliving@gmail.com>
2018-05-18 21:33:17 -0700

    commit to save

--- js/app.js
+++ js/app.js
@@ -54,14 +54,15 @@ jQuery(function ($) {
 		},
 		bindEvents: function () {
 			$('.new-todo').on('keyup', this.create.bind(this));
-			$('.toggle-all').on('change', this.toggleAll.bind(this));
-			$('.footer').on('click', '.clear-completed', this.destroyCompleted.bind(this));
-			$('.todo-list')
+			//$('.toggle-all').on('change', this.toggleAll.bind(this));
+			//$('.footer').on('click', '.clear-completed', this.destroyCompleted.bind(this));
+			/* $('.todo-list')
 				.on('change', '.toggle', this.toggle.bind(this))
 				.on('dblclick', 'label', this.editingMode.bind(this))
 				.on('keyup', '.edit', this.editKeyup.bind(this))
 				.on('focusout', '.edit', this.update.bind(this))
 				.on('click', '.destroy', this.destroy.bind(this));
+			*/
 		},
 		render: function () {
 			var todos = this.getFilteredTodos();
