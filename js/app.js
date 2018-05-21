5a1448d9e367790c69ec0340c40628acaf42b387
TheCodeWorm <4purposeliving@gmail.com>
2018-05-21 14:01:24 -0700

    commit to save

--- js/app.js
+++ js/app.js
@@ -1,29 +1,12 @@
-/*global jQuery, Handlebars, Router */
+
 jQuery(function ($) {
 	'use strict';
-
-	Handlebars.registerHelper('eq', function (a, b, options) {
-		return a === b ? options.fn(this) : options.inverse(this);
-	});
 
 	var ENTER_KEY = 13;
 	var ESCAPE_KEY = 27;
 
 	var util = {
-		uuid: function () {
-			var i, random;
-			var uuid = '';
-
-			for (i = 0; i < 32; i++) {
-				random = Math.random() * 16 | 0;
-				if (i === 8 || i === 12 || i === 16 || i === 20) {
-					uuid += '-';
-				}
-				uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
-			}
-
-			return uuid;
-		},
+		
 		store: function (namespace, data) {
 			if (arguments.length > 1) {
 				return localStorage.setItem(namespace, JSON.stringify(data));
@@ -36,35 +19,17 @@ jQuery(function ($) {
 
 	var App = {
 		init: function () {
-			this.todos = util.store('todos-jquery');
+			this.verses = util.store('verses-jquery');
 			this.bindEvents();
-
-			new Router({
-				'/:filter': function (filter) {
-					this.filter = filter;
-					this.render();
-				}.bind(this)
-			}).init('/all');
 		},
 		bindEvents: function () {
-			$('.new-todo').on('keyup', this.create.bind(this));
+			$('.new-verses').on('keyup', this.create.bind(this));
 		},
 		render: function () {
-			var todos = this.getFilteredTodos();
-			$('.new-todo').focus();
-			util.store('todos-jquery', this.todos);
+			$('.new-verses').focus();
+			util.store('verses-jquery', this.verses);
 		}, 
-		getFilteredTodos: function () {
-			if (this.filter === 'active') {
-				return this.getActiveTodos();
-			}
 
-			if (this.filter === 'completed') {
-				return this.getCompletedTodos();
-			}
-
-			return this.todos;
-		}, 
 		create: function (e) {
 			var $input = $(e.target);
 			var val = $input.val().trim().toLowerCase();
@@ -75,13 +40,8 @@ jQuery(function ($) {
 
 			let searchFor = val.split(" ");
 			setPassageLines(searchFor[0], searchFor[1]);
-			this.todos.splice(0,1);
 
-			this.todos.push({
-				id: util.uuid(),
-				title: val,
-				completed: false
-			});
+			this.verses = val;
 
 			$input.val('');
 
@@ -114,12 +74,6 @@ function setPassageLines(book, chapter) {  // verse, passage... Ex: rom 14
 	  	}
 		}
 
-		/*
-		for( let i=0; i< verses.length; i++) {
-	  	console.log(verses[i]);
-	  }
-	  */
-
 	  ////// Get passages from html:
 	  let passages = [];  // array for final extracted passages
 		// get results from html, for the passages of verses, 
@@ -131,12 +85,7 @@ function setPassageLines(book, chapter) {  // verse, passage... Ex: rom 14
 			resultsForPassages[i] = removeBrackets(resultsForPassages[i]);
 			resultsForPassages[i] = resultsForPassages[i].replace("&#8220;", '"').replace("&#8221;", '"').replace("&#8216;", '');
 			passages.push(resultsForPassages[i].replace("&#8217;", "'").replace("[fn]", "").replace("&#8201;", ''));
-			//console.log(passages[i]);
 		}
-    
-		console.log("test 1:");
-		console.log(verses);
-		console.log('test 2.....');
 
 		var passageLines = document.getElementById("passage-lines");
 
@@ -160,7 +109,6 @@ function setPassageLines(book, chapter) {  // verse, passage... Ex: rom 14
 		}
 		// clear placeholder 
 		document.getElementById("myInput1").placeholder = "";
-		console.log(passageLines);
 	});
 
 	// remove everything inside brackets, including brackets
